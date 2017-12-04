aaadf71b578dc5afe643bf113bb3bb49df993792
Paolo <mrtnpaolo@gmail.com>
2017-12-04 10:21:49 +0100

    🎄 day-04

---
+++ day-04/Anagrams.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.List (sort, nub)
+
+main = readFile "input.txt" >>= print . count . map words . lines
+
+count :: [[String]] -> Int
+count = sum . map (valid . map sort)
+
+valid :: [String] -> Int
+valid xs | sort xs == nub (sort xs) = 1
+         | otherwise = 0
