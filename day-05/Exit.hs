76c2dc5eefd2d90de087024d88e50e57a70e0259
Paolo <mrtnpaolo@gmail.com>
2017-12-05 16:10:44 +0100

    🎄 day-05 part 2
    so this time I rewrote the original code 😛

--- day-05/Exit.hs
+++ day-05/Exit.hs
@@ -3,12 +3,25 @@ module Main where
 
 import Data.List
 
-main = readFile "input.txt" >>= print . go . enter . map (read @Int) . lines
+main :: IO ()
+main = do
+  maze <- enter . map (read @Int) . lines <$> readFile "input.txt"
+  print $ go problem1 maze
+  print $ go problem2 maze
+
+problem1 = succ
+problem2 x | x > 2     = x-1
+           | otherwise = x+1
 
 data Maze a = In { left_ :: [a], at_ :: a, right_ :: [a] } | Out
-  deriving (Show)
+
+instance Show a => Show (Maze a) where
+  show Out = "Out"
+  show (In ls a rs) = concat $ intersperse " " $ tokens
+    where tokens = (map show . reverse) ls ++ ["(" ++ show a ++ ")"] ++ map show rs
 
 enter :: [a] -> Maze a
+enter []     = Out
 enter (x:xs) = In [] x xs
 
 -- Single movements
@@ -33,16 +46,17 @@ l :: Int -> Maze a -> Maze a
 l 0 m = m
 l n m = l (n-1) (left m)
 
--- Travel a maze, return the number of steps to get out
+-- Travel a maze using the given update function
+-- Returns the number of steps to get out
 
-go :: Maze Int -> Int
-go = fst . last . go' 0
+go :: (Int -> Int) -> Maze Int -> Int
+go update = fst . last . go' 0 update
 
-go' :: Int -> Maze Int -> [(Int,Maze Int)]
-go' n Out = [(n,Out)]
-go' n m@(In ls x rs) = (n,m) : go' (n+1) m''
+go' :: Int -> (Int -> Int) -> Maze Int -> [(Int,Maze Int)]
+go' n _      Out = [(n,Out)]
+go' n update m@(In ls x rs) = (n,m) : go' (n+1) update m''
   where
-    m' = (In ls (x+1) rs)
+    m' = (In ls (update x) rs)
     move | x >  0 = r x
          | x == 0 = id
          | x <  0 = l (-x)
